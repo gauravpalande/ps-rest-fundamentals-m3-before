e43ec879213ab799a01da83db70b111a57d7be60
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-06-09 11:48:28 -0700

    oders get

--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,25 @@
 import express from "express";
+import { parse } from "path";
+import { getOrders } from "./orders.service";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", async (req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+    if (
+        typeof take === "string" &&
+        parseInt(take) > 0 &&
+        skip &&
+        typeof skip === "string" &&
+        parseInt(skip) > -1
+    ) {
+        const orders = await getOrders(parseInt(skip), parseInt(take));
+        res.json(orders);
+    } else{
+        return res.status(400).json({ message: "Take and skip query parameters are required. " +
+            "Take must be greater than 0 and skip must be greater than -1",
+         });
+    }});
