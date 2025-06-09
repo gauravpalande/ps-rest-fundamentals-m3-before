550f11091d251f5069ce901b8b1ba4b02ee42aa5
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-06-09 11:01:25 -0700

    items GET

--- server/src/features/items/items.router.ts
+++ server/src/features/items/items.router.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import { get } from "http";
+import { getItems } from "./items.service";
 
 export const itemsRouter = express.Router();
+
+itemsRouter.get("/", async (req, res) => {
+  const items = await getItems();
+  items.forEach((item) => {
+    item.imageUrl = buildImageUrl(req, item.id);
+  });
+  res.json(items);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function buildImageUrl(req: any, id: number): string {
